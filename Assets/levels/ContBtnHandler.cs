2da61942919507b1f2930ca141b02d1696384f42
Taha Mohamed <61010487+taha309@users.noreply.github.com>
2026-09-16 03:50:24 +0300

    fix: resume El Mandoob gameplay and HUD cleanly

--- Assets/levels/ContBtnHandler.cs
+++ Assets/levels/ContBtnHandler.cs
@@ -1,25 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ContBtnHandler : MonoBehaviour
 {
     public GameObject pauseMenu;
     public GameObject pauseBtn;
-
     public GameObject joystick;
-    // public GameObject taskList;
     public HealthBar healthBar;
     public GameObject itemDelivered;
     public CountdownTimer timer;
+
     public void Continue()
     {
-        pauseMenu.SetActive(false);
-        // taskList.SetActive(true);
-        pauseBtn.SetActive(true);
-        joystick.SetActive(true);
-        healthBar.gameObject.SetActive(true);
-        itemDelivered.SetActive(true);
-        timer.counting = true;
+        Time.timeScale = 1f;
+
+        if (pauseMenu != null) pauseMenu.SetActive(false);
+        if (pauseBtn != null) pauseBtn.SetActive(true);
+        if (joystick != null) joystick.SetActive(true);
+        if (healthBar != null) healthBar.gameObject.SetActive(true);
+        if (itemDelivered != null) itemDelivered.SetActive(true);
+        if (timer != null) timer.counting = true;
+
+        Player player = FindObjectOfType<Player>();
+        if (player != null)
+        {
+            player.enabled = true;
+        }
+
+        ElMandoobHUD hud = FindObjectOfType<ElMandoobHUD>();
+        if (hud != null)
+        {
+            hud.SetPaused(false);
+        }
     }
 }
