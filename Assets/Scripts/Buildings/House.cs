dc66da6f1308e085f7f56606a3211a7d559eeedd
Taha Mohamed <61010487+taha309@users.noreply.github.com>
2026-09-16 03:35:10 +0300

    feat: add customer address metadata to delivery houses

--- Assets/Scripts/Buildings/House.cs
+++ Assets/Scripts/Buildings/House.cs
@@ -1,9 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class House : MonoBehaviour
 {
     [HideInInspector]
     public bool isDesination = false;
+
+    [HideInInspector]
+    public string customerName = "الزبون";
+
+    [HideInInspector]
+    public string address = "العنوان";
 }
