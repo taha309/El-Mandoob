e4b3e49aaf680541c9bc40aa1a0ac8079f21c7a3
Taha Mohamed <61010487+taha309@users.noreply.github.com>
2026-09-16 03:50:17 +0300

    fix: pause El Mandoob gameplay completely on desktop and mobile

--- Assets/levels/pauseBtnHandler.cs
+++ Assets/levels/pauseBtnHandler.cs
@@ -1,32 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class pauseBtnHandler : MonoBehaviour
 {
     public GameObject pauseMenu;
-
     public GameObject pauseBtn;
-
     public GameObject joystick;
-
     public GameObject taskList;
-
     public HealthBar healthBar;
     public GameObject itemsDelivered;
     public Button deliverButton, receiveButton;
     public CountdownTimer timer;
+
     public void pauseGame()
     {
-        taskList.SetActive(false);
-        pauseMenu.SetActive(true);
-        pauseBtn.SetActive(false);
-        joystick.SetActive(false);
-        healthBar.gameObject.SetActive(false);
-        itemsDelivered.SetActive(false);
-        deliverButton.gameObject.SetActive(false);
-        receiveButton.gameObject.SetActive(false);
-        timer.counting = false;
+        if (taskList != null) taskList.SetActive(false);
+        if (pauseMenu != null) pauseMenu.SetActive(true);
+        if (pauseBtn != null) pauseBtn.SetActive(false);
+        if (joystick != null) joystick.SetActive(false);
+        if (healthBar != null) healthBar.gameObject.SetActive(false);
+        if (itemsDelivered != null) itemsDelivered.SetActive(false);
+        if (deliverButton != null) deliverButton.gameObject.SetActive(false);
+        if (receiveButton != null) receiveButton.gameObject.SetActive(false);
+        if (timer != null) timer.counting = false;
+
+        Player player = FindObjectOfType<Player>();
+        if (player != null)
+        {
+            player.enabled = false;
+        }
+
+        ElMandoobHUD hud = FindObjectOfType<ElMandoobHUD>();
+        if (hud != null)
+        {
+            hud.SetPaused(true);
+        }
+
+        Time.timeScale = 0f;
     }
 }
