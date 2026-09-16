ffcd73146021e196d6e3cf36c59f1ea69a177a5d
Taha Mohamed <61010487+taha309@users.noreply.github.com>
2026-09-16 03:41:17 +0300

    fix: keep next shift navigation inside available El Mandoob levels

--- Assets/Scripts/NextLevelButton.cs
+++ Assets/Scripts/NextLevelButton.cs
@@ -1,14 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NextLevelButton : MonoBehaviour
 {
-    public void nextLevelButtonClick(){
-        int currentLevel = PlayerPrefs.GetInt("SelectedLevel");
-        PlayerPrefs.SetInt("SelectedLevel", currentLevel+1);
-        SceneManager.LoadScene((currentLevel+1-1)/4+1);
+    public void nextLevelButtonClick()
+    {
+        int currentLevel = PlayerPrefs.GetInt("SelectedLevel", 1);
+        if (currentLevel >= 8)
+        {
+            SceneManager.LoadScene(0);
+            return;
+        }
+
+        GameData data = SaveSystem.Load();
+        int nextLevel = Mathf.Min(currentLevel + 1, data.levelUnlocked);
+        nextLevel = Mathf.Clamp(nextLevel, 1, 8);
+
+        PlayerPrefs.SetInt("SelectedLevel", nextLevel);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene((nextLevel - 1) / 4 + 1);
     }
-    
 }
