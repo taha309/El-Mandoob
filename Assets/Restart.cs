f632612561aa8e377e0df30000a8fd4b407968d1
Taha Mohamed <61010487+taha309@users.noreply.github.com>
2026-09-16 03:50:49 +0300

    fix: restart shifts from a clean time state

--- Assets/Restart.cs
+++ Assets/Restart.cs
@@ -1,22 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement ;
+using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ResetTheGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void ResetTheGame(){
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
