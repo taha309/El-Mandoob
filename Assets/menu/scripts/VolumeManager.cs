b346cb148d381a965c0e72fd0ffc0d180aedd787
Taha Mohamed <61010487+taha309@users.noreply.github.com>
2026-09-16 03:41:11 +0300

    fix: load and persist El Mandoob audio settings safely

--- Assets/menu/scripts/VolumeManager.cs
+++ Assets/menu/scripts/VolumeManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -9,33 +7,66 @@ public class VolumeManager : MonoBehaviour
 
     public GameObject musicOn;
     public GameObject musicOff;
-
     public GameObject slider;
 
     public GameData data;
 
+    private void Start()
+    {
+        data = SaveSystem.Load();
+        AudioListener.volume = data.audios == 0 ? 0f : 1f;
+
+        if (mixer != null)
+        {
+            mixer.SetFloat("musicvol", data.volume);
+        }
+
+        if (musicOn != null)
+        {
+            musicOn.SetActive(data.audios != 0);
+        }
+        if (musicOff != null)
+        {
+            musicOff.SetActive(data.audios == 0);
+        }
+    }
+
     public void SetLevel(float sliderValue)
     {
-        mixer.SetFloat("musicvol", sliderValue);
+        EnsureData();
+        if (mixer != null)
+        {
+            mixer.SetFloat("musicvol", sliderValue);
+        }
         data.volume = sliderValue;
         SaveSystem.Save(data);
     }
 
     public void btnOn()
     {
+        EnsureData();
         AudioListener.volume = 0;
-        musicOn.SetActive(false);
-        musicOff.SetActive(true);
+        if (musicOn != null) musicOn.SetActive(false);
+        if (musicOff != null) musicOff.SetActive(true);
         data.audios = 0;
         SaveSystem.Save(data);
     }
 
     public void btnOff()
     {
+        EnsureData();
         AudioListener.volume = 1;
-        musicOn.SetActive(true);
-        musicOff.SetActive(false);
+        if (musicOn != null) musicOn.SetActive(true);
+        if (musicOff != null) musicOff.SetActive(false);
         data.audios = 1;
         SaveSystem.Save(data);
+    }
+
+    private void EnsureData()
+    {
+        if (data == null)
+        {
+            data = SaveSystem.Load();
+        }
     }
 }
