cfbd365092d7e6f0a886142483e75388258c4ecb
Taha Mohamed <61010487+taha309@users.noreply.github.com>
2026-09-16 03:35:05 +0300

    feat: add Egyptian business metadata to pickup shops

--- Assets/Scripts/Buildings/Shop.cs
+++ Assets/Scripts/Buildings/Shop.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    // Start is called before the first frame update
     [HideInInspector]
     public bool havingOrder = false;
+
+    [HideInInspector]
+    public string displayName = "محل";
 }
